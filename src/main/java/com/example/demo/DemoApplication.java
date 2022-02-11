794423fdabdd15a6f8763c5d1f03f5f260c724b4
Harivamsi1 <chharivamsi99@gmail.com>
2022-02-11 16:25:02 +0530

    kasi

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+	//deepak loverboy
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
